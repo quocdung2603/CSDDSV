6322f64bfedf42f2f21602489a3fa9a8fe269c06
TaiKonne <2124802010067@student.tdmu.edu.vn>
2024-03-17 18:33:29 +0700

    17/3/24 18h33

---
+++ src/Screens/ForumScreen/Forum.js
@@ -0,0 +1,27 @@
+import React, { useContext, useEffect, useState } from 'react';
+import {
+    Alert,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    Image,
+    TextInput,
+} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+const Forum = () =>{
+    return(
+        <View>
+            <Text>
+                forum
+            </Text>
+        </View>
+    )
+}
+export default Forum;
