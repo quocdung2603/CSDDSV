9cc447231bc7496551a2556bc37dbb2aab877a3e
TaiKonne <2124802010067@student.tdmu.edu.vn>
2024-03-18 16:23:45 +0700

    16:23PM push personal

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ import Home from './src/Screens/HomeScreen/Home'
 import Tabbar from './src/Navigation/Tabbar/Tabbar';
 import ProductList from './src/Screens/ProductScreen/ProductList';
 import CategoryMain from './src/Screens/CategoriesScreen/CategoryMain';
+import Profile from './src/Screens/PersonalScreen/Profile'
 LogBox.ignoreAllLogs();
 enableScreens(true)
-AppRegistry.registerComponent(appName, () => Tabbar);
+AppRegistry.registerComponent(appName, () => Profile);
 
