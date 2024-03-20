dae85b938142726877fa9f3f7c5c0d57c3ca2bb1
Dũng Nguyễn Quốc <nguyenquocdung26032003@gmail.com>
2024-03-20 16:23:45 +0700

    20/3/24 4:25PM push setting main screen

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import { AppRegistry, LogBox } from 'react-native';
 import { name as appName } from './app.json';
 import { enableScreens } from 'react-native-screens'
 import AppNavigator from './src/Navigation/AppNavigator';
+import SettingText from './src/Screens/SettingScreen/SettingText';
 LogBox.ignoreAllLogs();
 enableScreens(true)
-AppRegistry.registerComponent(appName, () => AppNavigator);
+AppRegistry.registerComponent(appName, () => SettingText);
 
--- src/Screens/SettingScreen/SettingText.js
+++ src/Screens/SettingScreen/SettingText.js
@@ -18,11 +18,61 @@ import Octicons from 'react-native-vector-icons/Octicons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Entypo from 'react-native-vector-icons/Entypo';
-
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 const SettingText = () => {
     return (
-        <View>
-            <Text>ABC</Text>
+        <View style={{ flex: 1, flexDirection: 'column', backgroundColor: '#fff' }}>
+            <View style={{ flexDirection: 'row', margin: 10, alignItems: 'center' }}>
+                <View style={{ borderWidth: 1, borderRadius: 10, padding: 5, marginEnd: 'auto' }}>
+                    <AntDesign name="arrowleft" size={30} color="#000" />
+                </View>
+                <Text style={{ marginEnd: 'auto', fontSize: 20, color: '#000', fontWeight: 'bold' }}>Cài Đặt</Text>
+                <View style={{ marginStart: 'auto' }}>
+                </View>
+            </View>
+            <View style={{ flexDirection: 'column', margin: 40}}>
+                <Image
+                    style={{ height: 150, width: 150, borderRadius: 75, justifyContent:'center', alignSelf:'center'}}
+                    source={require("../../../Img/Dong_Doan.jpg")} />
+            </View>
+            <View style={{flexDirection:'column', margin:10}}>
+                <TouchableOpacity 
+                        onPress={() => {}}
+                        style={{flexDirection:'row', marginHorizontal:10, alignItems:'center'}}>
+                    <View style={{borderWidth:1, borderRadius:50, marginEnd:10}}>
+                        <AntDesign name="user" size={27} color='#000' />
+                    </View>
+                    <Text style={{fontSize:22, fontWeight:'bold',color:'#000', marginEnd:'auto'}}>Thông Tin Cá Nhân</Text>
+                </TouchableOpacity>
+                <View style={{borderWidth:0.2, margin:10}}/>
+                <TouchableOpacity 
+                        onPress={() => {}}
+                        style={{flexDirection:'row', marginHorizontal:10, alignItems:'center'}}>
+                    <View style={{borderWidth:1, borderRadius:50, marginEnd:10}}>
+                        <AntDesign name="user" size={27} color='#000' />
+                    </View>
+                    <Text style={{fontSize:22, fontWeight:'bold',color:'#000', marginEnd:'auto'}}>Tài Khoản</Text>
+                </TouchableOpacity>
+                <View style={{borderWidth:0.2, margin:10}}/>
+                <TouchableOpacity 
+                        onPress={() => {}}
+                        style={{flexDirection:'row', marginHorizontal:10, alignItems:'center'}}>
+                    <View style={{borderWidth:1, borderRadius:50, marginEnd:10}}>
+                        <MaterialIcons name="manage-accounts" size={27} color='#000' />
+                    </View>
+                    <Text style={{fontSize:22, fontWeight:'bold',color:'#000', marginEnd:'auto'}}>Bảo Mật</Text>
+                </TouchableOpacity>
+                <View style={{borderWidth:0.2, margin:10}}/>
+                <TouchableOpacity 
+                        onPress={() => {}}
+                        style={{flexDirection:'row', marginHorizontal:10, alignItems:'center'}}>
+                    <View style={{borderWidth:1, borderRadius:50, marginEnd:10}}>
+                        <AntDesign name="Safety" size={27} color='#000' />
+                    </View>
+                    <Text style={{fontSize:22, fontWeight:'bold',color:'#000', marginEnd:'auto'}}>Đăng Xuất</Text>
+                </TouchableOpacity>
+                {/* <View style={{borderWidth:0.2, margin:10}}/> */}
+            </View>
         </View>
     )
 }
