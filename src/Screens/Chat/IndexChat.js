0147c340dd9438c4f9c88690f015709f35a79c62
Dũng Nguyễn Quốc <nguyenquocdung26032003@gmail.com>
2024-04-14 11:04:02 +0700

    14/4/24 11:03 AM

--- src/Screens/Chat/IndexChat.js
+++ src/Screens/Chat/IndexChat.js
@@ -51,42 +51,55 @@ const IndexChat = ({ navigation }) => {
             })
         console.log(listChat)
     }
-
+    // biến, const
+    const [SC, setSC] = useState(0);
     return (
-        <View style={{flex: 1, flexDirection:'column'}}>
-            <View style={{flexDirection: 'row',alignItems: 'center', marginVertical:10}}>
-                <AntDesign name='caretleft' size={30} color={'#FCBB3C'} style={{marginEnd:'auto', marginStart:10}}/>
+        <View style={{ flex: 1, flexDirection: 'column' }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 10 }}>
+                <TouchableOpacity onPress={() => {navigation.goBack()}} style={{ marginEnd: 'auto', marginStart: 10 }} >
+                    <AntDesign name='caretleft' size={30} color={'#FCBB3C'}/>
+                </TouchableOpacity>
+                <Text style={{fontSize:25, fontWeight:'bold', color:'#FCCC6F'}}>BOX CHAT</Text>
+                <View style={{marginStart:'auto', marginEnd:10}}></View>
             </View>
-            {/* <View style={{borderWidth:1}}>
+            <View style={{}}>
                 <ScrollView>
                     <FlatList
                         data={listChat}
                         renderItem={({ item, index }) => {
                             return (
                                 <>
-                                    
+                                    <View style={{ flexDirection: 'row', alignItems: 'center', marginHorizontal: 10, marginVertical: 5, padding: 10, borderWidth: 1, borderRadius: 10 }}>
+                                        <View style={{ width: 40, height: 40, backgroundColor: 10, borderRadius: 20, backgroundColor: 'yellow' }}></View>
+                                        <View style={{ flexDirection: 'column', marginStart: 10 }}>
+                                            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Nguyễn Quốc Dũng</Text>
+                                            <Text style={{ fontSize: 13, fontWeight: 'bold' }}>10:43 PM</Text>
+                                        </View>
+                                        <TouchableOpacity
+                                            onPress={() => { SC === 0 ? setSC(1) : setSC(0) }}
+                                            style={{ borderWidth: 1, borderRadius: 10, padding: 5, marginStart: 'auto' }}>
+                                            <Entypo name='dots-three-horizontal' size={20} color='#000' />
+                                        </TouchableOpacity>
+                                    </View>
+                                    {SC === 1 ? (
+                                        <View style={{ flexDirection: 'row', alignItems: 'center', padding: 5, marginHorizontal: 20 }}>
+                                            <TouchableOpacity
+                                                style={{ flexDirection: 'row', alignItems: 'center', marginHorizontal: 10, padding: 2, borderWidth: 1, borderRadius: 10, borderColor: '#FCCC6F', backgroundColor: '#FCCC6F' }}>
+                                                <Entypo name='lock' size={25} color='#000' />
+                                                <Text style={{ color: '#000', fontWeight: 'bold' }}>Khóa tin nhắn</Text>
+                                            </TouchableOpacity>
+                                            <TouchableOpacity
+                                                style={{ flexDirection: 'row', alignItems: 'center', marginHorizontal: 10, padding: 2, borderWidth: 1, borderRadius: 10, borderColor: '#FCCC6F', backgroundColor: '#FCCC6F' }}>
+                                                <Entypo name='lock' size={25} color='#000' />
+                                                <Text style={{ color: '#000', fontWeight: 'bold' }}>Xóa tin nhắn</Text>
+                                            </TouchableOpacity>
+                                        </View>
+                                    ) : ""}
                                 </>
                             )
                         }}
                     />
                 </ScrollView>
-            </View> */}
-            <View style={{ flexDirection: 'row', alignItems: 'center', marginHorizontal: 10, marginVertical: 5, padding: 10, borderWidth: 1, borderRadius: 10 }}>
-                <View style={{ width: 40, height: 40, backgroundColor: 10, borderRadius: 20, backgroundColor: 'yellow' }}></View>
-                <View style={{flexDirection:'column', marginStart:10}}>
-                    <Text style={{ fontSize: 16, fontWeight: 'bold'}}>Nguyễn Quốc Dũng</Text>
-                    <Text style={{ fontSize: 13, fontWeight: 'bold'}}>10:43 PM</Text>
-                </View>
-                <TouchableOpacity
-                    onPress={() => { Alert.alert("Xem chi tiết người dùng") }}
-                    style={{ borderWidth: 1, borderRadius: 10, padding: 5, marginStart: 'auto', marginEnd: 10 }}>
-                    <Ionicons name='eye' size={20} color='#000' />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => { Alert.alert("Khóa tài khoản người dùng") }}
-                    style={{ borderWidth: 1, borderRadius: 10, padding: 5 }}>
-                    <Octicons name='blocked' size={20} color='#000' />
-                </TouchableOpacity>
             </View>
         </View>
     )
