b974024c0af49ef2be5d9f5d422548ffb1cf0d71
TaiKonne <2124802010067@student.tdmu.edu.vn>
2024-05-01 14:46:26 +0700

    add map

--- src/Test1/Index.js
+++ src/Test1/Index.js
@@ -1,28 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
 
-import CheckBox from '@react-native-community/checkbox';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
-import Octicons from 'react-native-vector-icons/Octicons';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Entypo from 'react-native-vector-icons/Entypo';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-
-import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
-import { Picker } from '@react-native-picker/picker'
-import firestore from '@react-native-firebase/firestore';
-import storage from '@react-native-firebase/storage';
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import uuid from 'react-native-uuid'
 import MapView from 'react-native-maps';
 
 const Index = () => {
-    return (
-        <View>
 
+    return (
+        <View
+            style={{
+                flex: 1,
+                borderWidth: 1,
+                borderColor: 'black',
+            }}
+        >
+            <MapView
+                initialRegion={{
+                    latitude: 11.136039,
+                    longitude: 106.701686,
+                    latitudeDelta: 0.1,
+                    longitudeDelta: 0.1,
+                }}
+                // style={{
+                //     width: Dimensions.get('window').width,
+                //     height: Dimensions.get('window').height,
+                // }}
+                style={{
+                    width: '100%',
+                    height: '100%',
+                }}
+            />
         </View>
     )
 }
